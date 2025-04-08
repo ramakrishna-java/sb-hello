52602680bda5b2beee2cfe9bce8ef5d3112108e1
unknown <1267.rk@gmail.com>
2025-04-09 00:51:30 +0530

    modifications

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "hi hello world ..." + name;
+    }
+    
     @GetMapping("/hello")
-    public String sayHello() {
+    public String sayHelloGuest() {
         return "hi hello world ...";
     }
 }
